03146b0fa60eb124aa49d4ffa0c989dbb4dcf778
wu <wu673@purdue.edu>
2016-01-31 20:57:54 -0500

    add collectionView

---
+++ Movie review/CollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionCell.swift
+//  Movie review
+//
+//  Created by New on 1/31/16.
+//  Copyright © 2016 New. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    
+}
